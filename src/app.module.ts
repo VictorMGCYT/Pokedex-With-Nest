ea568887eed80ebeea44de5b7b36212b27b32914
VictorMGCYT <manuelvgc2233@gmail.com>
2025-02-23 12:08:14 -0600

    Making DB with Docker and MongoDB
    23/02/2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
+
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // Importanción para servir archivos estaticos de la carpeta public
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }),
+    PokemonModule 
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Genéra un prefijo para buscar la URL de la API
+  app.setGlobalPrefix('api/v2')
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
