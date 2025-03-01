fee54f4da82274e4cd3eedd4b235d4801f76fc5c
VictorMGCYT <manuelvgc2233@gmail.com>
2025-02-28 21:07:54 -0600

    fetching pokemons from pokeapi
    28.02.2025

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   
+  private readonly axios: AxiosInstance = axios
 
-  executeSeed(){
-    return `Seed has been executed successfully`;
+  async executeSeed(){
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=20  ')
+
+    const pokemons: {name: string, id: string}[]  = []
+    data.results.forEach( ({name, url}) => {
+      const segments = url.split('/');
+      const id = segments[6];
+      
+      pokemons.push({name, id})
+
+    })
+    console.log(pokemons);
+    return data.results;
   }
   
 }
